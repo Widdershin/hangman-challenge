e1e9855e9a237e65607fc7d2eacc0c8b573ab04a
Nick Johnstone <ncwjohnstone@gmail.com>
2015-06-01 01:02:34 +1200

    Iteration 2: Guessing

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,34 @@
+# Hangman Pseudocode
+# Make an empty array to keep track of successful guesses
+#
+# Loop
+#   Display the word to the user (with underscores for each letter not guessed)
+#   Ask the user to guess a letter
+#
+#   If the letter guessed is in the word to guess
+#     Add the guess to the list of successful guesses
+
 
 word_to_guess = File.readlines('words.txt').map { |word| word.strip }.sample
+letters_successfully_guessed = []
 
-# Hangman Pseudocode
-# Display the word to the user (with underscores for each letter not guessed)
+loop do
 
-word_to_display = word_to_guess.chars.map { |character| "_" }.join(" ")
+  word_to_display = word_to_guess.chars.map do |character|
+    if letters_successfully_guessed.include? character
+      character
+    else
+      "_"
+    end
+  end.join(" ")
 
-puts "Word to guess: #{word_to_display}"
+  puts "Word to guess: #{word_to_display}"
+
+  puts "Guess a letter:"
+
+  letter_guessed = gets.strip
+
+  if word_to_guess.include? letter_guessed
+    letters_successfully_guessed << letter_guessed
+  end
+end
