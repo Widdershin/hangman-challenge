9018b5271da92d99c5d73191b1d4c91d3c740127
Nick Johnstone <ncwjohnstone@gmail.com>
2015-06-01 01:04:44 +1200

    Iteration 3: lives!

--- hangman.rb
+++ hangman.rb
@@ -1,16 +1,21 @@
 # Hangman Pseudocode
 # Make an empty array to keep track of successful guesses
+# Start with 7 lives remaining
 #
 # Loop
 #   Display the word to the user (with underscores for each letter not guessed)
+#   Display the number of lives remaining
 #   Ask the user to guess a letter
 #
 #   If the letter guessed is in the word to guess
 #     Add the guess to the list of successful guesses
+#   else
+#     lose a life
 
 
 word_to_guess = File.readlines('words.txt').map { |word| word.strip }.sample
 letters_successfully_guessed = []
+lives_remaining = 7
 
 loop do
 
@@ -22,6 +27,7 @@ loop do
     end
   end.join(" ")
 
+  puts "Lives remaining: #{lives_remaining}"
   puts "Word to guess: #{word_to_display}"
 
   puts "Guess a letter:"
@@ -30,5 +36,7 @@ loop do
 
   if word_to_guess.include? letter_guessed
     letters_successfully_guessed << letter_guessed
+  else
+    lives_remaining -= 1
   end
 end
