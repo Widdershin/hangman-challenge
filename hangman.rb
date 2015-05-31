e1eee4996159f3cb15ab4e0325910ab5c8ea8a0b
Nick Johnstone <ncwjohnstone@gmail.com>
2015-06-01 00:56:16 +1200

    Iteration 1: Underscores

--- hangman.rb
+++ hangman.rb
@@ -1,2 +1,9 @@
 
 word_to_guess = File.readlines('words.txt').map { |word| word.strip }.sample
+
+# Hangman Pseudocode
+# Display the word to the user (with underscores for each letter not guessed)
+
+word_to_display = word_to_guess.chars.map { |character| "_" }.join(" ")
+
+puts "Word to guess: #{word_to_display}"
